c3c7a8e1e5f9992f8187356c1e749eb3345642b1
Rhaven Cometa <49508041+rhavendc@users.noreply.github.com>
2026-04-18 01:31:50 +0000

    Added setters+getters

--- Users.java
+++ Users.java
@@ -5,10 +5,66 @@ public class Users {
     String name;
     String address;
     double balance;
-}
-//team panda: generate constructors and getters+setters
-//team panda eof
 
+    //team panda: generate constructors and getters+setters
+    public Users(int id, String mobileNumber, int pin, String name, String address, double balance) {
+        this.id = id;
+        this.mobileNumber = mobileNumber;
+        this.pin = pin;
+        this.name = name;
+        this.address = address;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    //team panda  eof
+}
 //pata team : generate login
 //pata team  eof
 
